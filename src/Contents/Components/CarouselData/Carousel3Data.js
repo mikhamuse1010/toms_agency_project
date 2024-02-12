b84b11ff75c851281b8e8890991372926c8284ca
mikhamuse1010 <75568270+mikhamuse1010@users.noreply.github.com>
2024-02-12 22:37:54 +0700

    update packages

---
+++ src/Contents/Components/CarouselData/Carousel3Data.js
@@ -0,0 +1,17 @@
+const Carousel3Data = [
+    {
+        title: "UI Soup",
+        Links: "",
+        desktopImage: Desktop1,
+        mobileImage: Mobile1,
+    },
+    {
+        title: "Goldcrown Labs",
+        Links: "",
+        desktopImage: Desktop2,
+        mobileImage: Mobile2,
+    }
+
+]
+
+export default Carousel2Data;
---
+++ src/Contents/Components/CarouselData/dist/Carousel3Data.dev.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var Carousel3Data = [{
+  title: "UI Soup",
+  Links: "",
+  desktopImage: Desktop1,
+  mobileImage: Mobile1
+}, {
+  title: "Goldcrown Labs",
+  Links: "",
+  desktopImage: Desktop2,
+  mobileImage: Mobile2
+}];
+var _default = Carousel2Data;
+exports["default"] = _default;
